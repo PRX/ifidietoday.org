23659c0f014aed669fa592f1f699d57b354c9f1c
Chris Kalafarski <chris@farski.com>
2012-06-06 12:03:47 -0400

    form

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -1,3 +1,44 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '371339856249007', // App ID
+    channelUrl : '', // Channel File
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true // parse XFBML
+  });
+
+  FB.Event.subscribe('auth.login', function(response) {
+    window.location = window.location;
+  });
+
+  FB.Canvas.setAutoGrow();
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+$('a.facebook').click(function() {
+  FB.login(function(response) {
+    if (response.authResponse) {
+      FB.api('/me', function(response) {
+        if (response.location) {
+          alert('Yes locaction');
+        } else {
+          $('body').addClass('form');
+        }
+      });
+    } else {
+      alert('Did not get authorization');
+    }
+  }, {perms:'user_likes,user_photos,user_photo_video_tags,user_birthday,user_location'});
+});
+
+
 $(function() {
   var numModules = $('#modules section').length;
 
